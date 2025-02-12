b96c5e13925c1a45bf8355b478cbd1222f1cb7d4
Zonakkis <2720716602@qq.com>
2025-02-12 20:39:46 +0800

    引入依赖注入

--- GOILauncher/App.axaml.cs
+++ GOILauncher/App.axaml.cs
@@ -4,27 +4,38 @@ using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
 using GOILauncher.ViewModels;
 using GOILauncher.UI.Views;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 
 namespace GOILauncher
 {
     public partial class App : Application
     {
         public static TopLevel? TopLevel { get; private set; }
+        public IServiceProvider ServiceProvider { get; private set; }
         public override void Initialize()
         {
             AvaloniaXamlLoader.Load(this);
         }
         public override void OnFrameworkInitializationCompleted()
         {
+            var services = new ServiceCollection();
+            ConfigureServices(services);
+            ServiceProvider = services.BuildServiceProvider();
             if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
             {
                 desktop.MainWindow = new MainWindow
                 {
-                    DataContext = new MainWindowViewModel(),
+                    DataContext = ServiceProvider.GetRequiredService<MainWindowViewModel>(),
                 };
                 TopLevel = TopLevel.GetTopLevel(desktop.MainWindow);
             }
             base.OnFrameworkInitializationCompleted();
+        }
+
+        private void ConfigureServices(ServiceCollection services)
+        {
+            services.AddSingleton<MainWindowViewModel>();
         }
     }
 }
