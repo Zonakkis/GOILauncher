ce61ac26f3052bcdd980c12613b9d97faed50695
Zonakkis <2720716602@qq.com>
2025-03-01 17:29:10 +0800

    update字段映射更改

--- GOILauncher/Models/Update.cs
+++ GOILauncher/Models/Update.cs
@@ -1,8 +1,14 @@
-﻿namespace GOILauncher.Models;
+﻿using System.Text.Json.Serialization;
+
+namespace GOILauncher.Models;
 
 public class Update
 {
+
+    [JsonPropertyName("version")]
     public string Version { get; init; }
+    [JsonPropertyName("url")]
     public string Url { get; init; }
+    [JsonPropertyName("changelog")]
     public string Changelog { get; init; }
 }
